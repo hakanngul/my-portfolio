7fc8125cf568bd81ca37357e6deb0e193f6f31fe
Hakan Gul <hakanngul@icloud.com>
2025-06-28 23:10:12 +0000

    resume eklendi

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "HAKAN GUL",
-  profile: '/profile.png',
+  profile: '/profile.jpg',
   designation: "Senior Software Developer in Test Engineer",
   description: "My name is Hakan Gul. I am a passionate and experienced QA Engineer and Test Automation specialist with over 4 years of professional experience. I specialize in building scalable test automation frameworks using Java, C#, Selenium, and modern CI/CD practices. I have led QA teams, mentored junior engineers, and successfully reduced manual testing efforts by up to 65% through innovative automation solutions. I'm passionate about quality assurance, continuous improvement, and delivering reliable software products. Currently working as a Senior Software Developer in Test Engineer at Nesine.com, where I lead automation initiatives for high-traffic betting platforms.",
   email: 'hakanngul@icloud.com',
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: '',
   leetcode: "",
   devUsername: "",
-  resume: ""
+  resume: "https://hakangul.my.canva.site"
 }
